f705a845dfa09e7e6ab2f07f0a15ed2b7640d7af
Alonso <iaquintanapa@gmail.com>
2023-08-21 20:52:28 -0500

    recuperacion de registros de datos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Course;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        
+
+        $courses = Course::where('status', '3')->latest('id')->get();
+
+        return view('welcome', compact('courses'));
     }
 }
