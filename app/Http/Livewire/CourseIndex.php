699c748283b9ecb224c7ca8e99283d572be8df02
Alonso <iaquintanapa@gmail.com>
2023-08-30 08:46:21 -0500

    un peuenyo avance

--- app/Http/Livewire/CourseIndex.php
+++ app/Http/Livewire/CourseIndex.php
@@ -5,11 +5,15 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 use App\Models\Course;
+use App\Models\Category;
+use App\Models\Level;
 
 class CourseIndex extends Component
 {
     public function render()
     {
+
+        $categories = Category
 
         $courses = Course::where('status', 3)->latest('id')->paginate(8);
 
