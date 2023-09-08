a955be6e7fd6018944cb021c2d5d1cb81a996c11
Alonso <iaquintanapa@gmail.com>
2023-09-07 20:50:49 -0500

    estilos en la pagina finalizado

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -1,42 +1,89 @@
 <div class="mt-8">
     <div class="max-w-7xl mx-auto px-4 grid grid-cols-3 gap-8">
-        <div class="col-span-2 text-gray-900">
-            {!!$current->iframe!!}
-            {{$current->name}}
-            <p>Indice: {{$this->index}}</p>
-            <p>Previous: @if ($this->previous)
-                {{$this->previous->id}}
-            @endif</p>
-            <p>Next: @if ($this->next)
-                {{$this->next->id}}
-            @endif</p>
+        <div class="col-span-2">
+            <div class="position-relative overflow-hidden pt-56.25%">
+                {!!$current->iframe!!}
+            </div>
+            
+            <h1 class="text-3xl text-gray-600 font-bold mt-4">
+                {{$current->name}}
+            </h1>
+
+            @if ($current->description)
+                <div class="text-gray-600">
+                    {{$current->description->name}}
+                </div>
+            @endif
+
+            <div class="flex items-center mt-4 cursor-pointer text-gray-900">
+                <i class="fas fa-toggle-off text-2xl text-gray-600"></i>
+                <p class="text-sm ml-2">Marcar esta unidad como culminada</p>
+            </div>
+
+            <div class="bg-white shadow-lg rounded overflow-hidden mt-2">
+                <div class="px-6 py-4 flex text-gray-500 font-bold">
+                    
+                    @if ($this->previous)
+                        <a wire:click="changeLesson({{$this->previous}})" class="cursor-pointer">Tema anterior</a>
+                    @endif
+
+
+                    @if ($this->next)
+                        <a wire:click="changeLesson({{$this->next}})" class="ml-auto cursor-pointer">Siguiente tema</a>
+                    @endif
+                </div>
+            </div>
+
         </div>
 
         <div class="bg-white shadow-lg rounded overflow-hidden">
-            <div class="px-6 py-4">
-                <h1 class="text-gray-900">{{$course->title}}</h1>
+            <div class="px-6 py-4 text-gray-900">
+                <h1 class="text-2xl leading-8 text-center mb-4">{{$course->title}}</h1>
 
                 <div class="flex items-center">
                     <figure>
-                        <img src="{{$course->teacher->profile_photo_url}}" alt="">
+                        <img class="w-12 h-12 object-cover rounded-full mr-4" src="{{$course->teacher->profile_photo_url}}" alt="">
                     </figure>
 
                     <div class="text-gray-900">
                         <p>{{$course->teacher->name}}</p>
-                        <a class="text-blue-500" href="">{{'@' . Str::slug($course->teacher->name, '')}}</a>
+                        <a class="text-blue-500 text-sm" href="">{{'@' . Str::slug($course->teacher->name, '')}}</a>
+                    </div>
+                </div>
+
+                <p class="text-gray-600 text-sm mt-2">20% completado</p>
+
+                <div class="relative pt-1">
+                    <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-200">
+                      <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
                     </div>
                 </div>
 
                 <ul class="text-gray-900">
                     @foreach ($course->sections as $section)
-                        <li>
-                            <a class="font-bold">{{$section->name}}</a>
+                        <li class="text-gray-600 mb-4">
+                            <a class="font-bold text-base inline-block mb-2">{{$section->name}}</a>
                             <ul>
                                 @foreach ($section->lessons as $lesson)
-                                    <li>
-                                        <a class="cursor-pointer" wire:click="changeLesson({{$lesson}})" >{{$lesson->id}} @if ($lesson->completed)
-                                            (Completado)
-                                        @endif</a>
+                                    <li class="flex">
+                                        <div>
+                                            @if ($lesson->completed)
+
+                                                @if ($current->id == $lesson->id)
+                                                    <span class="inline-block w-4 h-4 border-2 border-yellow-300 rounded-full mr-2 mt-1"></span>
+                                                @else
+                                                    <span class="inline-block w-4 h-4 bg-yellow-300 rounded-full mr-2 mt-1"></span>
+                                                @endif
+                                                
+                                            @else
+                                                @if ($current->id == $lesson->id)
+                                                    <span class="inline-block w-4 h-4 border-2 border-gray-500 rounded-full mr-2 mt-1"></span>
+                                                @else
+                                                    <span class="inline-block w-4 h-4 bg-gray-500 rounded-full mr-2 mt-1"></span>
+                                                @endif
+                                            @endif
+                                        </div>
+                                        <a class="cursor-pointer" wire:click="changeLesson({{$lesson}})" >{{$lesson->name}}</a>
                                     </li>
                                 @endforeach
                             </ul>
