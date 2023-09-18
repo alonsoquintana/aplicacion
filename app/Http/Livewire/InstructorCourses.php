157c6055c421526fa3d56143450a7e226a0a88e3
Alonso <iaquintanapa@gmail.com>
2023-09-18 08:19:40 -0500

    mostrado de un curso en la seccion del instructor

--- app/Http/Livewire/InstructorCourses.php
+++ app/Http/Livewire/InstructorCourses.php
@@ -4,10 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\Course;
+use Livewire\WithPagination;
+
 class InstructorCourses extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.instructor-courses');
+        $courses = Course::where('user_id', auth()->user()->id)->paginate(8);
+
+        return view('livewire.instructor-courses', compact('courses'));
     }
 }
